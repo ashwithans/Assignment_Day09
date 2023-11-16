c4d54b9adf82ddc4d31516ddd6c17704492225c3
ashwithans <149990261+ashwithans@users.noreply.github.com>
2023-11-16 15:34:22 +0530

    Program.cs

--- ConAppDay9Assignment/Program.cs
+++ ConAppDay9Assignment/Program.cs
@@ -1,4 +1,3 @@
-﻿using MemoryManagementExample;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,10 +12,29 @@ namespace ConAppDay9Assignment
         {
             LargeDataCollection largeCollection = new LargeDataCollection(new int[] { 1, 2, 3, 4, 5 });
 
-            largeCollection.AddElement(6);
-            largeCollection.RemoveElement(3);
+
+            // Display initial elements
+            Console.WriteLine("Initial elements in the collection:");
             largeCollection.AccessElements();
+
+            // Manually add an element
+            Console.Write("Enter an element to add: ");
+            int elementToAdd = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Adding element: {elementToAdd}");
+            largeCollection.AddElement(elementToAdd);
+
+            Console.Write("Enter an element to remove: ");
+            int elementToRemove = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Removing element: {elementToRemove}");
+            largeCollection.RemoveElement(elementToRemove);
+
+            // Display elements after manual operations
+            Console.WriteLine("Elements in the collection after modifications:");
+            largeCollection.AccessElements();
+
+            // Dispose the collection
             largeCollection.Dispose();
+            Console.ReadKey();
         }
     }
 }
