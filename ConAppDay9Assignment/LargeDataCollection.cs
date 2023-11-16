cb5cde1cedcee741871aa5417b99dfe547271af1
ashwithans <ashwithans@gmail.com>
2023-11-16 14:40:06 +0530

    Assignment_Day09

---
+++ ConAppDay9Assignment/LargeDataCollection.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MemoryManagementExample
+{
+    public class LargeDataCollection : IDisposable
+    {
+        private List<int> dataCollection;
+
+        public LargeDataCollection(IEnumerable<int> initialData)
+        {
+            dataCollection = new List<int>(initialData);
+            DisplayInitialData(initialData);
+        }
+
+        public void AddElement(int element)
+        {
+            dataCollection.Add(element);
+            Console.WriteLine($"Added element: {element}");
+        }
+
+        public bool RemoveElement(int element)
+        {
+            bool removed = dataCollection.Remove(element);
+            if (removed)
+            {
+                Console.WriteLine($"Removed element: {element}");
+            }
+            else
+            {
+                Console.WriteLine($"Element {element} not found to remove.");
+            }
+            return removed;
+        }
+
+        public void AccessElements()
+        {
+            Console.WriteLine("Elements in the collection:");
+            foreach (var item in dataCollection)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public void Dispose()
+        {
+            dataCollection.Clear();
+            dataCollection = null;
+            GC.Collect();
+        }
+
+        private void DisplayInitialData(IEnumerable<int> initialData)
+        {
+            Console.WriteLine("Initial data entered:");
+            foreach (var item in initialData)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ ConAppDay9Assignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using MemoryManagementExample;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppDay9Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LargeDataCollection largeCollection = new LargeDataCollection(new int[] { 1, 2, 3, 4, 5 });
+
+            largeCollection.AddElement(6);
+            largeCollection.RemoveElement(3);
+            largeCollection.AccessElements();
+            largeCollection.Dispose();
+        }
+    }
+}
